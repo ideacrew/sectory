3780cce860fafe4127a42ea133bf8c08a7be8c59
Trey Evans <lewis.r.evans@gmail.com>
2024-07-05 16:30:22 -0400

    Test runs.

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule Sectory.MixProject do
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      test: ["ecto.create", "ecto.migrate", "test"],
       "assets.setup": ["esbuild.install --if-missing"],
       "assets.build": ["esbuild sectory"],
       "assets.deploy": [
--- test/sectory_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SectoryWeb.PageControllerTest do
-  use SectoryWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
